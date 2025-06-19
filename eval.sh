d348eb87e4bd697923922fad0810ed49fecffc1f
张声涛 <13996479+zhang-shengtao123@user.noreply.gitee.com>
2025-06-20 02:01:24 +0800

    eval手算更新

--- eval.sh
+++ eval.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH -o llava.%j.out               # 脚本执行的输出将被保存在 llava.%j.out文件下，%j表示作业号
+#SBATCH -o eval.%j.out               # 脚本执行的输出将被保存在 eval.%j.out文件下，%j表示作业号
 #SBATCH --partition=l40s
 #SBATCH --qos=dcgpu                    # 指定作业的 QOS
-#SBATCH -J llava                      # 作业在调度系统中的作业名为 llava
+#SBATCH -J eval                      # 作业在调度系统中的作业名为 llava
 #SBATCH --nodes=1                     # 申请节点数为 1
 #SBATCH --ntasks-per-node=1           # 每个节点上运行的任务数为 4
-#SBATCH --gres=gpu:3                  # 指定作业需要的 GPU卡数量为 4
+#SBATCH --gres=gpu:1                  # 指定作业需要的 GPU卡数量为 4
 #SBATCH --cpus-per-task=8             # 每个任务分配的 CPU核心数
 # SBATCH --nodelist=l40sgpu001        # 指定作业只能在 node1和 node2节点上执行
 # a100gpu003,l40gpu001
@@ -17,7 +17,11 @@ nvidia-smi
 # ==== 调试信息 ====
 echo "Python 路径: $(which python)"
 
-python eval.py
+python eval.py \
+    --model-path /lab/zhangjg_lab/30028000/llava/models/llava-v1.5-7b \
+    --coco-val-annotations-file /lab/zhangjg_lab/30028000/llava/data/coco/annotations/captions_val2014.json \
+    --image-folder /lab/zhangjg_lab/30028000/llava/data/coco/val2014 \
+    --num-test-images 3200 # 你可以调整测试图片数量
 
 
 echo "LLaVA LoRA 微调测试结束。"
